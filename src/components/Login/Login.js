e8995a5f5a95018a3def3cdd470d509cb5e8d3e5
Ryan Bas <ryan.basmajian@forgerock.com>
2021-09-15 12:23:14 -0600

    task(remove-kba): KBA is part of the registration
    Remove KBA step from login journey

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,7 +3,6 @@ import { Box, VStack, FormControl } from 'native-base';
 import { NativeModules } from 'react-native';
 
 import { Password } from './Password';
-import { KBA } from './KBA';
 import { Footer } from './Footer';
 import { Username } from './Username';
 import { Loggedin } from './Loggedin';
@@ -18,23 +17,17 @@ const map = {
   PasswordCallback: ({ label, setter }) => (
     <Password setPass={setter} label={label} key={label} />
   ),
-  KbaCreateCallback: ({ label, setter }) => (
-    <KBA setAnswer={setter} label={label} key={label} />
-  ),
 };
 
 function Login({ data, callbacks, navigation }) {
   const [username, setUsername] = useState('');
-  const [ans, setAnswer] = useState('');
   const [pass, setPass] = useState('');
-  const [kba, setKba] = useState(null);
   const [err, setErr] = useState(null);
   const [res, setRes] = useState(null);
   const [user, setUser] = useState(null);
 
   const typeMap = {
     PasswordCallback: pass,
-    KbaCreateCallback: ans,
     NameCallback: username,
   };
 
@@ -46,7 +39,6 @@ function Login({ data, callbacks, navigation }) {
 
   const setterMap = {
     PasswordCallback: setPass,
-    KbaCreateCallback: setAnswer,
     NameCallback: setUsername,
   };
 
@@ -55,7 +47,6 @@ function Login({ data, callbacks, navigation }) {
       case 'LoginSuccess': {
       }
       case 'LoginFailure': {
-        console.log('err', err);
         setErr('Incorrect credentials, try again');
       }
       default: {
@@ -63,31 +54,9 @@ function Login({ data, callbacks, navigation }) {
     }
   }, [res]);
 
-  const handleKbaSubmit = async () => {
-    const res = kba.callbacks.map(({ type, response }) => {
-      const res = JSON.parse(response);
-      res.input[1].value = typeMap[type];
-      return res;
-    });
-
-    const stringifiedResponse = JSON.stringify({ ...kba, callbacks: res });
-
-    try {
-      const response = await ForgeRockModule.next(stringifiedResponse);
-
-      if (response.type === 'LoginSuccess') {
-        loginSuccess(setUser);
-      }
-    } catch (error) {
-      setErr(error.message);
-      setRes(error.message);
-    }
-  };
-
   const handleLogout = async () => {
     await ForgeRockModule.performUserLogout();
     setUser(null);
-    setKba(null);
     setUsername(null);
     setPass(null);
   };
@@ -103,10 +72,9 @@ function Login({ data, callbacks, navigation }) {
       const response = await ForgeRockModule.next(stringifiedResponse);
       if (response.type === 'LoginSuccess') {
         loginSuccess(setUser);
-      } else {
-        setKba(JSON.parse(response));
       }
     } catch (error) {
+      console.log(error.message);
       setRes(error.message);
     }
   };
@@ -132,17 +100,7 @@ function Login({ data, callbacks, navigation }) {
                   : null,
               )
             : null}
-          {kba && kba.callbacks
-            ? kba.callbacks.map(({ type, prompt: label }) =>
-                map[type]
-                  ? map[type]({
-                      label,
-                      setter: setterMap[type],
-                    })
-                  : null,
-              )
-            : null}
-          <Footer handleSubmit={kba ? handleKbaSubmit : handleSubmit} />
+          <Footer handleSubmit={handleSubmit} />
         </VStack>
       </FormControl>
     </Box>
