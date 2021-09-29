5d9b4940a874bcfc576e1af1764c9e9bbf3beb2c
George Bafaloukas <george.bafaloukas@forgerock.com>
2021-09-29 11:24:27 +0100

    Added MaterialCommunityIcons.ttf font and installed react-native-vector-icons for the iOS project Added example icons for the TabBar

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -7,6 +7,7 @@ import { Login, Todos, Register } from '../screens';
 import Loading from '../components/utilities/loading';
 import { AppContext, useGlobalStateMgmt } from '../global-state';
 import { useToggle } from '../hooks/useToggle';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const { ForgeRockModule } = NativeModules;
 const Tab = createBottomTabNavigator();
@@ -65,13 +66,42 @@ function RootNavigator() {
   console.log(isAuthenticated);
   return isAuthenticated ? (
     <Tab.Navigator initialRoute="Todos">
-      <Tab.Screen name="Todos" component={Todos} />
-      <Tab.Screen name="Logout" component={Logout} />
+      <Tab.Screen 
+      name="Todos" 
+      component={Todos} 
+      options={{
+          tabBarLabel: 'Todos',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="format-list-checks" color={color} size={size} />
+          ),
+        }}/>
+      <Tab.Screen name="Logout" 
+      component={Logout} 
+      options={{
+        tabBarLabel: 'Logout',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="logout-variant" color={color} size={size} />
+        ),
+      }}/>
     </Tab.Navigator>
   ) : (
     <Tab.Navigator initialRoute="Login">
-      <Tab.Screen name="Login" component={Login} />
-      <Tab.Screen name="Register" component={Register} />
+      <Tab.Screen name="Login" 
+      component={Login} 
+      options={{
+        tabBarLabel: 'Login',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="login-variant" color={color} size={size} />
+        ),
+      }}/>
+      <Tab.Screen name="Register" 
+      component={Register} 
+      options={{
+        tabBarLabel: 'Register',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="account-plus" color={color} size={size} />
+        ),
+      }}/>
     </Tab.Navigator>
   );
 }
